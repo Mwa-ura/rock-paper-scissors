2f5d1870ca889d67710f15a83d911dfc118dbf29
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-11-17 20:03:42 +0400

    Update js file

---
+++ rpsGame.js
@@ -0,0 +1,25 @@
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
+//console.log(getComputerChoice());
+
+function game(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return 'It is a tie!';
+        }
+        else if ((playerSelection==='rock' && computerSelection==='scissors') || 
+        (playerSelection==='scissors' && computerSelection==='paper') ||
+        (playerSelection==='rock' && computerSelection==='paper')){
+            return `You win! ${playerSelection} beat ${computerSelection}`
+        }
+        else {
+            return `You lose! ${computerSelection} beat ${playerSelection}`
+        }
+}
+var playerChoice= prompt('Enter your choice.').toLowerCase();
+//console.log(playerChoice);
+var computerChoice = getComputerChoice();
+//console.log(computerChoice)
+console.log(game(playerChoice, computerChoice));
