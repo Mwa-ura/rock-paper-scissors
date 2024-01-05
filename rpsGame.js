fec1ee430078996e6df32029bac16eec23ffd7c1
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2024-01-05 13:59:57 +0400

    Update.

--- rpsGame.js
+++ rpsGame.js
@@ -5,23 +5,68 @@ function getComputerChoice() {
 }
 //console.log(getComputerChoice());
 
-function game(playerSelection, computerSelection) {
-    for (let i = 0; i < 5; i++) {
-        if (playerSelection === computerSelection) {
-            return 'It is a tie!';
-        }
-        else if ((playerSelection==='rock' && computerSelection==='scissors') || 
+ /* function game(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log('It is a tie!');
+    }
+    else if ((playerSelection==='rock' && computerSelection==='scissors') || 
         (playerSelection==='scissors' && computerSelection==='paper') ||
         (playerSelection==='rock' && computerSelection==='paper')){
-            return `You win! ${playerSelection} beat ${computerSelection}`
-        }
-        else {
-            return `You lose! ${computerSelection} beat ${playerSelection}`
-        }
+            console.log(`You win! ${playerSelection} beat ${computerSelection}`)
+     }
+    else {
+        console.log(`You lose! ${computerSelection} beat ${playerSelection}`)
     }
-}
-var playerChoice= prompt('Enter your choice.').toLowerCase();
+ }
+ */
+
+/*
 //console.log(playerChoice);
 var computerChoice = getComputerChoice();
 //console.log(computerChoice)
-console.log(game(playerChoice, computerChoice));
+//console.log(
+let count = 1;
+for (let i = 0; i < count; i++) {
+    var playerChoice = prompt('Enter your choice.').toLowerCase();
+    game(playerChoice, computerChoice);
+}
+*/
+const container = document.querySelector('#container');
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+rockButton.style.marginRight = '4px';
+/*
+// rockButton.addEventListener('click', )
+const rockPress = () => {
+    if (playerSelection==='rock' && (computerSelection==='scissors' || computerSelection==='paper')) {
+        console.log(`You win! ${playerSelection} beat ${computerSelection}`); 
+    }
+
+};
+
+// paperButton eventListener
+const scissorPress = () => {
+    if ((playerSelection==='scissors' && computerSelection==='paper')) {
+        console.log(`You win! ${playerSelection} beat ${computerSelection}`);
+    }
+};
+
+// scissorsButton
+const paperPress = () => {
+    if (playerSelection === 'paper' && (computerChoice==='rock' || computerSelection==='scissors')) {
+        console.log(`You lose! ${playerSelection} beat ${computerSelection}`);
+    }
+}
+*/
+
+container.appendChild(rockButton);
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+paperButton.value = 'paper';
+console.log(paperButton.value);
+paperButton.style.marginRight = '4px';
+container.appendChild(paperButton);
+const scissorButton = document.createElement('button');
+scissorButton.textContent = 'Scissors';
+scissorButton.style.marginRight = '4px';
+container.appendChild(scissorButton);
